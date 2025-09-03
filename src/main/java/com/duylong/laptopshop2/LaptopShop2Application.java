385572e22a8df6656c5680879f3602960fb7a565
Duy Long <longngo2407@gmail.com>
2025-09-03 13:56:34 +0700

    First commit

---
+++ src/main/java/com/duylong/laptopshop2/LaptopShop2Application.java
@@ -0,0 +1,13 @@
+package com.duylong.laptopshop2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopShop2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopShop2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/duylong/laptopshop2/LaptopShop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duylong.laptopshop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopShop2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
