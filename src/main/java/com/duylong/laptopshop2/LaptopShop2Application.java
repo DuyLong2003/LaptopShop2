c58548e6f30fbd04d54a0d50cae449d15db7f718
Duy Long <longngo2407@gmail.com>
2025-09-12 15:46:25 +0700

    Spring Security

--- src/main/java/com/duylong/laptopshop2/LaptopShop2Application.java
+++ src/main/java/com/duylong/laptopshop2/LaptopShop2Application.java
@@ -2,6 +2,7 @@ package com.duylong.laptopshop2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopShop2Application {
@@ -10,4 +11,12 @@ public class LaptopShop2Application {
 		SpringApplication.run(LaptopShop2Application.class, args);
 	}
 
+	// In ra tất cả bean có trong Spring IoC
+	// container
+	// ApplicationContext duylong =
+	// SpringApplication.run(LaptopShop2Application.class, args);
+	// for (String s : duylong.getBeanDefinitionNames()) {
+	// System.out.println(s);
+	// }
+	// }
 }
